3b79214ff97e34eb06ba9d7753ca9e01fc882d01
u1986970 <u1986970@campus.udg.edu>
2025-07-18 19:04:32 +0200

    pilot a nau i viceversa

--- Corrupted Chronos/Assets/Scripts/Nau/Player.cs
+++ Corrupted Chronos/Assets/Scripts/Nau/Player.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Ink.Parsed;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
-public class Moviment : MonoBehaviour
+public class Player : MonoBehaviour
 {
     
     
@@ -13,28 +16,47 @@ public class Moviment : MonoBehaviour
     private Rigidbody rb;
     private Collider myCollider;
     //[SerializeField] 
-    private float checkRadius = 0.6f;
+    private float checkRadius = 0.5f;
     [SerializeField] 
     private LayerMask collisionMask;
     
     //Controls/inputs
-    private PlayerInput playerInput;
-    private PlayerInputActions playerInputActions;
+    //private PlayerInput playerInput;
+    public PlayerInputActions playerInputActions;
 
     //moure capa
     //no segur del private, recomanció de rider
     private Collider[] hitsAbove;
     private Collider[] hitsBelow;
+    
+    //altres scripts
+    InteractiveMethods interactiveMethods;
+    List<InteractionType> whatsToInteract;
+
+    //demoment serialitzat, pero es probable que quan això es torni més complexe s'hagui de passar per codi i no per inspector
+    [SerializeField]
+    private GameObject _nau;
+    [SerializeField]
+    private GameObject _pilot;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         myCollider = GetComponent<Collider>();
         if (myCollider == null){ Debug.LogError("No collider attached!"); return; }
-        playerInput = GetComponent<PlayerInput>();
+        //playerInput = GetComponent<PlayerInput>();
         
         playerInputActions = new PlayerInputActions();
         playerInputActions.Nau.Enable();
+        _nau.SetActive(true);
+        playerInputActions.Global.Enable();
+        playerInputActions.Global.Interactua.performed += Interact;
+        
         playerInputActions.Nau.CanviCapa.performed += moveCapa;
+
+        interactiveMethods= new InteractiveMethods();
+        whatsToInteract = new List<InteractionType>();
+        
     }
 
 
@@ -44,44 +66,71 @@ public class Moviment : MonoBehaviour
 
     }
 
+    void Update()
+    {
+
+        if (playerInputActions.Nau.enabled)
+        {
+            _nau.SetActive(true);
+            _pilot.SetActive(false);
+        }else if (playerInputActions.Pilot.enabled)
+        {
+            _nau.SetActive(false);
+            _pilot.SetActive(true);
+        }
+    }
+
     void FixedUpdate()
     {
+
+        if (playerInputActions.Nau.enabled)
+        {
+            MoveNau();
+        }else if (playerInputActions.Pilot.enabled)
+        {
+            MovePilot();
+        }
+        
+    }
+
+
+    void MoveNau()
+    {
+        //Debug.Log("estàs amb la nau");
+
         rb.linearVelocity = Vector3.zero;
-        float spawnsp= 0.65f;
-        //si necesito actualitzar això en el menú, a fixed Update
+        
+        float spawnsp= 0.5f;
         hitsAbove = Physics.OverlapSphere(new Vector3(rb.position.x,rb.position.y+spawnsp,rb.position.z), checkRadius, collisionMask);
         hitsBelow = Physics.OverlapSphere(new Vector3(rb.position.x,rb.position.y-spawnsp,rb.position.z), checkRadius, collisionMask);
 
-        Vector2 movement =playerInputActions.Nau.Move.ReadValue<Vector2>().normalized;
+        //això del playerInputAction
+        Vector2 movement =playerInputActions.Nau.MoveNau.ReadValue<Vector2>().normalized;
         float moveSpeed = 5f;
         //transform.Translate(new Vector3(movement.x, 0, movement.y));
 
         rb.MovePosition(rb.position+ new Vector3(movement.x, 0, movement.y) * moveSpeed *Time.deltaTime);
-    }
-    void OnDrawGizmos()
-    {
-        if (rb == null) return; // Avoid errors if not set in Inspector
-        Vector3 targetPos = rb.position + Vector3.down;
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(targetPos, checkRadius);
-        Vector3 targetPos1 = rb.position + Vector3.up;
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(targetPos1, checkRadius);
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    void MovePilot()
     {
-        //float moveX = Input.GetAxisRaw("Horizontal");
-        //float moveZ = Input.GetAxisRaw("Vertical");
-        //movement = new Vector3(moveX, 0f, moveZ).normalized;
+        //Debug.Log("estàs amb el pilot");
+        
+        rb.linearVelocity = Vector3.zero;
+        Vector2 movement =playerInputActions.Pilot.MovePilot.ReadValue<Vector2>().normalized;
+        float moveSpeed = 2f;
+        //transform.Translate(new Vector3(movement.x, 0, movement.y));
 
+        rb.MovePosition(rb.position+ new Vector3(movement.x, 0, movement.y) * moveSpeed *Time.deltaTime);
+        
     }
+    
 
     private void moveCapa(InputAction.CallbackContext context)
     {
        
-        Debug.Log("rodeta ratolí detectat");
+        //Debug.Log("rodeta ratolí detectat");
         float movement = playerInputActions.Nau.CanviCapa.ReadValue<float>();
         
         if (movement > 0 )
@@ -132,16 +181,39 @@ public class Moviment : MonoBehaviour
         //Debug.Log("canvi capa"+ movement);
 
     }
-
-    /*public void movementinputAAA(InputAction.CallbackContext context)
+    void OnDrawGizmos() //els detectors de col·lisió basicament
     {
-        Debug.Log("move input detected");
-        if (context.performed)
-        {
-            Vector2 inputVector = context.ReadValue<Vector2>();
-            rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * moveSpeed, ForceMode.Force);
-        }
-        //Vector2 inputVector = RTeadValue<Vector2>();
-        //rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * moveSpeed, ForceMode.Force);
-    }*/
+        if (rb == null) return; // Avoid errors if not set in Inspector
+        Vector3 targetPos = rb.position + Vector3.down;
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(targetPos, checkRadius);
+        Vector3 targetPos1 = rb.position + Vector3.up;
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(targetPos1, checkRadius);
+    }
+
+    
+    
+    
+    
+    //Interacció amb objectes del escenari o coses especials
+    void Interact(InputAction.CallbackContext context)
+    {
+        Debug.Log("you press E");
+        interactiveMethods.DoInteractions(whatsToInteract,this);
+        
+    }
+    public void AddInteraction(InteractionType type)
+    {
+        Debug.Log("added");
+        whatsToInteract.Add(type);
+    }
+    
+    public void SubInteraction(InteractionType type)
+    {
+        whatsToInteract.Remove(type);
+    }
+    
+    
+    
 }
