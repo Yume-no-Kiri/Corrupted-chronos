d6030f93af11e219bba19faeac365caef3da81c6
Lepommejamez <98928651+Lepommejamez@users.noreply.github.com>
2026-05-08 12:58:02 +0200

    More changes! Towers implemented

---
+++ Corrupted Chronos/Assets/Scripts/Enemies/Boss/BossSpawner.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour
+{
+    public static BossSpawner instance;
+    public List<GameObject> bossTowers = new List<GameObject>();
+
+
+    [Header("Kraken Parameters")]
+    public GameObject bossPrefab;
+    public Transform spawnPoint;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void towerDestroyed(GameObject obj)
+    {
+        if (obj == null || bossTowers == null || bossTowers.Count == 0)
+            return;
+
+        bossTowers.Remove(obj);
+
+        if (bossTowers.Count <= 0)
+        {
+            spawnBoss();
+        }
+    }
+
+    public void spawnBoss()
+    {
+        Instantiate(bossPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
